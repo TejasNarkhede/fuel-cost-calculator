064d9ce40f3cd87c70134bcb21159a2cc5d6bfac
TejasNarkhede <tejasnarkhede999@gmail.com>
2025-02-26 17:01:47 +0530

    tailwind css & shadcn ui setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export default {
+    mode: 'jit',
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
